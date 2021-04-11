32683a90ecc0f73c1ff8b6cbdc2ed680fa28fd40
tristan <westenskowtristan122@gmail.com>
2021-04-11 13:00:12 -0600

    added pathways to server.js, and notes to db.json

--- server.js
+++ server.js
@@ -2,5 +2,39 @@ const fs = require("fs");
 const path = require("path");
 const { notes } = require("./db/db.json");
 const express = require("express");
+const { json } = require("body-parser");
 const port = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  let results = notes;
+  res.json(results);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.post("/api/notes", (req, res) => {
+  req.body.id = notes.length.toString();
+  const newNote = req.body;
+  notes.push(newNote);
+  fs.writeFileSync(
+    path,
+    json(__dirname, "./db/db.json"),
+    JSON.stringify({ notes }, null, 2)
+  );
+  res.json(newNote);
+});
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}`);
+});
