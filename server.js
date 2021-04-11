9e0797224670aa4d94a0da2211a9a7d7d4cb8ed6
tristan <westenskowtristan122@gmail.com>
2021-04-11 13:19:43 -0600

    pushing to heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const { notes } = require("./db/db.json");
 const express = require("express");
 const { json } = require("body-parser");
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
