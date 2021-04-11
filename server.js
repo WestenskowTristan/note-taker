f22a59a1daa229a4c831261836d8439cd5b6f0fa
tristan <westenskowtristan122@gmail.com>
2021-04-11 13:56:40 -0600

    fixing bugs

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const { notes } = require("./db/db.json");
 const express = require("express");
-const { json } = require("body-parser");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
